eb34a9c05ce0699bca99f1ddb3c92b346fc54b06
Cory Buecker <email@corybuecker.com>
2015-12-26 07:33:05 -0600

    refactor fake db into separate package

--- stats.go
+++ stats.go
@@ -13,8 +13,15 @@ import (
 )
 
 func main() {
+	session, err := gorethink.Connect(gorethink.ConnectOpts{Address: "localhost:28015"})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	var rethinkDB database.RethinkDB
+	rethinkDB = database.RethinkDB{Session: session}
 
-	var config = new(configuration.Configuration)
+	var config configuration.Configuration
+	config = configuration.Configuration{}
 
 	if err := config.Load("./config.json"); err != nil {
 		log.Fatal(err)
@@ -23,13 +30,9 @@ func main() {
 	var steamFetcher = &steam.Fetcher{SteamAPIKey: config.SteamAPIKey, SteamID: config.SteamID}
 	var giantBombFetcher = &giantbomb.Fetcher{GiantBombAPIKey: config.GiantBombAPIKey}
 
-	session, err := gorethink.Connect(gorethink.ConnectOpts{Address: "localhost:28015"})
-
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-
-	rethinkDB := database.RethinkDB{Session: session}
 
 	storage.Setup(&rethinkDB, "videogames", []string{"ownedgames", "giantbomb"})
 
---
+++ test/rethinkdb.go
@@ -0,0 +1,41 @@
+package test
+
+type FakeDatabase struct {
+	Entry             map[string]interface{}
+	DatabaseCreated   string
+	ExistingDatabases []string
+	TableCreated      string
+	ExistingTables    []string
+}
+
+func (rethinkDB *FakeDatabase) Upsert(databaseName string, tableName string, record map[string]interface{}) error {
+	rethinkDB.Entry = record
+	return nil
+}
+
+func (rethinkDB *FakeDatabase) CreateTable(databaseName string, tableName string) error {
+	rethinkDB.TableCreated = tableName
+	return nil
+}
+func (rethinkDB *FakeDatabase) CreateDatabase(databaseName string) error {
+	rethinkDB.DatabaseCreated = databaseName
+	return nil
+}
+func (rethinkDB *FakeDatabase) ListDatabases() ([]string, error) {
+	return rethinkDB.ExistingDatabases, nil
+}
+func (rethinkDB *FakeDatabase) ListTables(databaseName string) ([]string, error) {
+	return rethinkDB.ExistingTables, nil
+}
+
+func (rethinkDB *FakeDatabase) RowsWithoutField(databaseName string, tableName string, fieldToExclude string) ([]map[string]interface{}, error) {
+	return []map[string]interface{}{
+		{
+			"name": "mario",
+		},
+	}, nil
+}
+
+func (rethinkDB *FakeDatabase) GetRow(databaseName string, tableName string, field string) (map[string]interface{}, error) {
+	return nil, nil
+}
