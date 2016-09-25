ff63dcd974704c204ce2f950f2fa649b7823afe4
Cory Buecker <email@corybuecker.com>
2016-09-25 13:46:05 -0500

    rename main

---
+++ steamfetcher.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+
+	"github.com/corybuecker/steamfetcher/actions"
+	"github.com/urfave/cli"
+)
+
+func main() {
+	var databaseHost string
+
+	app := cli.NewApp()
+
+	app.Name = "steamfetcher"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "host",
+			Value:       "localhost",
+			Usage:       "connection host for MongoDB",
+			Destination: &databaseHost,
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "steam",
+			Usage: "update all owned games from steam",
+			Action: func(c *cli.Context) error {
+				if err := actions.UpdateSteam(databaseHost); err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+
+		{
+			Name:  "wikipedia",
+			Usage: "search for wikipedia pages for all owned games",
+			Action: func(c *cli.Context) error {
+				if err := actions.SearchWikipedia(databaseHost); err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
